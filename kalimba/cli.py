b323f6bce829b6ec8c65a1a38466a4e8a64ce320
Dmitrii Orlov <dmtree.dev@yahoo.com>
2023-09-20 15:12:56 +0200

    feat(wip): non-blocking toggle button & -v mode

--- kalimba/cli.py
+++ kalimba/cli.py
@@ -10,14 +10,14 @@ logger.setLevel(l.INFO)
 APP_NAME = "kalimba"
 
 
-def __run_detached_kalimba():
+def __run_detached_kalimba(*_):
     l.info(f"starting {APP_NAME} in detached mode")
     raise NotImplementedError("Sorry... this is not yet implemented")
 
 
-def __run_foreground_kalimba():
+def __run_foreground_kalimba(verbose: bool):
     l.info(f"starting {APP_NAME} in foreground mode")
-    run_kalimba()
+    run_kalimba(verbose=verbose)
 
 
 @click.command()
@@ -26,12 +26,20 @@ def __run_foreground_kalimba():
     default=False,
     help="Run the Kalimba process in a detached (sub-process) or foreground mode",
 )
-def start(detached: bool):
+@click.option(
+    "-v",
+    "--verbose",
+    is_flag=True,
+    default=False,
+    help="Run the Kalimba process with extended logging",
+)
+@click.pass_context
+def start(detached: bool, verbose: bool):
     """Simple starter for Kalimba"""
     if detached:
-        __run_detached_kalimba()
+        __run_detached_kalimba(verbose)
     else:
-        __run_foreground_kalimba()
+        __run_foreground_kalimba(verbose)
 
 
 if __name__ == "__main__":
--- kalimba/kalimba.py
+++ kalimba/kalimba.py
@@ -5,16 +5,31 @@ A simple Menu Bar app to control your colimba status.
 import rumps
 import subprocess
 import logging as l
+from concurrent.futures import ThreadPoolExecutor
 
 l.basicConfig()
 logger = l.getLogger(__name__)
-# TODO(backlog): make log level configurable
-logger.setLevel(l.DEBUG)
 
 
 class StatusMenuItem(rumps.MenuItem):
     def __init__(self):
         super(StatusMenuItem, self).__init__("👀 status")
+
+
+class ToggleMenuItem(rumps.MenuItem):
+    DEFAULT_TITLE = "Toggle"
+
+    def __init__(self):
+        super(ToggleMenuItem, self).__init__(self.DEFAULT_TITLE)
+
+    def set_starting(self) -> None:
+        self.title = "Starting ..."
+
+    def set_stopping(self) -> None:
+        self.title = "Stopping ..."
+
+    def reset(self) -> None:
+        self.title = self.DEFAULT_TITLE
 
 
 class KalimbaMenuBarApp(rumps.App):
@@ -23,17 +38,24 @@ class KalimbaMenuBarApp(rumps.App):
     def __init__(self):
         super(KalimbaMenuBarApp, self).__init__("Kalimba", "🐳")
         self.__status_menu = StatusMenuItem()
-        self.menu = [self.__status_menu, "Toggle"]
+        self.__toggle_menu = ToggleMenuItem()
+        self.menu = [self.__status_menu, self.__toggle_menu]
+        self.__colima_cli_single_pool = ThreadPoolExecutor(max_workers=1)
 
-    @rumps.clicked("Toggle")
-    def start(self, _):
-        # TODO(tech-debt): make this call not-blocking but thread safe
-        cmd = self.__get_colima_trigger_cmd()
-        self.__toggle_colima(cmd)
+    # FIXME: the cleanup should be called on any exit of the App
+    def cleanup(self) -> None:
+        self.__colima_cli_single_pool.shutdown()
 
     # TODO(backlog): make the timer configurable
     @rumps.timer(30)
-    def check_status(self, _):
+    def check_status(self, _) -> None:
+        self.__check_status_handler()
+
+    @rumps.clicked(ToggleMenuItem.DEFAULT_TITLE)
+    def toggle(self, _) -> None:
+        self.__colima_cli_single_pool.submit(self.__toggle_handler)
+
+    def __check_status_handler(self) -> None:
         try:
             result = subprocess.run(
                 ["colima", "status"],
@@ -49,19 +71,24 @@ class KalimbaMenuBarApp(rumps.App):
             self.__update_status_menu_title(status_title, status_icon)
             self.__update_colima_run_status(colima_running)
         except Exception as error:
-            logger.error(error)
-            self.__update_status_menu_title("failed to get status...", "🤕")
+            self.__handle_error(error)
 
-    def __update_status_menu_title(self, title: str, icon: str):
-        self.__status_menu.title = icon + " " + title
+    def __toggle_handler(self) -> None:
+        try:
+            if self.__colima_running:
+                self.__toggle_menu.set_stopping()
+                self.__toggle_colima("stop")
+                self.__update_colima_run_status(False)
+            else:
+                self.__toggle_menu.set_starting()
+                self.__toggle_colima("start")
+                self.__update_colima_run_status(True)
+        except Exception as error:
+            self.__handle_error(error)
+        finally:
+            self.__toggle_menu.reset()
 
-    def __update_colima_run_status(self, colima_running: bool):
-        self.__colima_running = colima_running
-
-    def __get_colima_trigger_cmd(self) -> str:
-        return "stop" if self.__colima_running else "start"
-
-    def __toggle_colima(self, cmd: str):
+    def __toggle_colima(self, cmd: str) -> None:
         try:
             result = subprocess.run(["colima", cmd])
             result.check_returncode()
@@ -69,12 +96,24 @@ class KalimbaMenuBarApp(rumps.App):
             logger.error(error)
             rumps.alert("failed to toggle colima...", "🤕")
 
+    def __update_status_menu_title(self, title: str, icon: str) -> None:
+        self.__status_menu.title = icon + " " + title
+
+    def __update_colima_run_status(self, colima_running: bool) -> None:
+        self.__colima_running = colima_running
+
+    def __handle_error(self, error: Exception) -> None:
+        logger.error(error)
+        self.__update_status_menu_title("failed to get status...", "🤕")
+        self.__update_colima_run_status(False)
+
 
 # mem(docs-link): update README if this changes.
-def run_kalimba():
-    logger.info('Starting the kalimba app... 🎶')
+def run_kalimba(verbose=False):
+    logger.setLevel(l.DEBUG if verbose else l.INFO)
+    logger.info("Starting the kalimba app... 🎶")
     KalimbaMenuBarApp().run()
 
 
 if __name__ == "__main__":
-    run_kalimba()
+    run_kalimba(verbose=True)
