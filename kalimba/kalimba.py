0f53afd54b6b81856ec53af7be6a0f6088aa3bc9
Dmitrii Orlov <dmtree.dev@yahoo.com>
2023-10-10 10:18:53 +0200

    chore: code reorganization & aquatic icons

--- kalimba/kalimba.py
+++ kalimba/kalimba.py
@@ -11,9 +11,32 @@ l.basicConfig()
 logger = l.getLogger(__name__)
 
 
+class KalimbaIcons:
+    WHALE = "🐳"
+    LOOKING = "👀"
+    SLEEP = "🐚"
+    RUN = "🐠"
+    BROKEN = "🙊"
+    TUNE = "🎶"
+
+
+class KalimbaMessages:
+    APP_TITLE = "Kalimba"
+    APP_STARTING = f"Starting the kalimba app... {KalimbaIcons.TUNE}"
+    COLIMA_RUNNING = "colima is running"
+    COLIMA_STARTING = "Starting ..."
+    COLIMA_STOPPING = "Stopping ..."
+    FAILED_TOGGLE = "failed to toggle colima..."
+    FAILED_STATUS = "failed to get status..."
+
+
+class KalimbaThreadPoolExecutor(ThreadPoolExecutor):
+    pass
+
+
 class StatusMenuItem(rumps.MenuItem):
     def __init__(self):
-        super(StatusMenuItem, self).__init__("👀 status")
+        super(StatusMenuItem, self).__init__(f"${KalimbaIcons.LOOKING} status")
 
 
 class ToggleMenuItem(rumps.MenuItem):
@@ -23,24 +46,54 @@ class ToggleMenuItem(rumps.MenuItem):
         super(ToggleMenuItem, self).__init__(self.DEFAULT_TITLE)
 
     def set_starting(self) -> None:
-        self.title = "Starting ..."
+        self.title = KalimbaMessages.COLIMA_STARTING
 
     def set_stopping(self) -> None:
-        self.title = "Stopping ..."
+        self.title = KalimbaMessages.COLIMA_STOPPING
 
     def reset(self) -> None:
         self.title = self.DEFAULT_TITLE
 
 
+class ColimaCli:
+    __cli__ = "colima"
+
+    @classmethod
+    def get_status(cls) -> tuple:
+        opts = dict(text=True, check=False, capture_output=True)
+        result = subprocess.run([cls.__cli__, "status"], **opts)
+        output = result.stdout or result.stderr
+        output_lines = output.splitlines()
+        logger.debug(output_lines)
+        status_title = output_lines[0]
+        return status_title, output_lines[1:]
+
+    @classmethod
+    def start_engine(cls) -> bool:
+        cls.__toggle_engine("start")
+
+    @classmethod
+    def stop_engine(cls) -> bool:
+        cls.__toggle_engine("stop")
+
+    @classmethod
+    def __toggle_engine(cls, cmd: str) -> None:
+        result = subprocess.run([cls.__cli__, cmd])
+        result.check_returncode()
+
+
 class KalimbaMenuBarApp(rumps.App):
     __colima_running = False
+    __colima_cli = ColimaCli
 
     @staticmethod
-    def prepare_cli_pool() -> ThreadPoolExecutor:
-        return ThreadPoolExecutor(max_workers=1)
+    def prepare_cli_pool() -> KalimbaThreadPoolExecutor:
+        return KalimbaThreadPoolExecutor(max_workers=1)
 
-    def __init__(self, colima_cli_pool: ThreadPoolExecutor):
-        super(KalimbaMenuBarApp, self).__init__("Kalimba", "🐳")
+    def __init__(self, colima_cli_pool: KalimbaThreadPoolExecutor):
+        super(KalimbaMenuBarApp, self).__init__(
+            KalimbaMessages.APP_TITLE, KalimbaIcons.WHALE
+        )
         self.__colima_cli_single_pool = colima_cli_pool
         self.__status_menu = StatusMenuItem()
         self.__toggle_menu = ToggleMenuItem()
@@ -52,21 +105,16 @@ class KalimbaMenuBarApp(rumps.App):
 
     def __check_status_handler(self) -> None:
         try:
-            result = subprocess.run(
-                ["colima", "status"],
-                text=True,
-                check=False,
-                capture_output=True,
-            )
-            output = result.stdout or result.stderr
-            logger.debug(output.splitlines())
-            status_title = output.splitlines()[0]
-            colima_running = "colima is running" in status_title
-            status_icon = "🏃" if colima_running else "😴"
+            status_title, _ = self.__colima_cli.get_status()
+            colima_running = KalimbaMessages.COLIMA_RUNNING in status_title
+            status_icon = KalimbaIcons.RUN if colima_running else KalimbaIcons.SLEEP
             self.__update_status_menu_title(status_title, status_icon)
             self.__update_colima_run_status(colima_running)
         except Exception as error:
-            self.__handle_error(error)
+            logger.error(error)
+            alert_args = (KalimbaMessages.FAILED_STATUS, KalimbaIcons.BROKEN)
+            self.__update_status_menu_title(*alert_args)
+            self.__update_colima_run_status(False)
 
     @rumps.clicked(ToggleMenuItem.DEFAULT_TITLE)
     def queued_toggle(self, _) -> None:
@@ -76,24 +124,19 @@ class KalimbaMenuBarApp(rumps.App):
         try:
             if self.__colima_running:
                 self.__toggle_menu.set_stopping()
-                self.__toggle_colima("stop")
+                self.__colima_cli.stop_engine()
                 self.__update_colima_run_status(False)
             else:
                 self.__toggle_menu.set_starting()
-                self.__toggle_colima("start")
+                self.__colima_cli.start_engine()
                 self.__update_colima_run_status(True)
         except Exception as error:
-            self.__handle_error(error)
+            logger.error(error)
+            alert_args = (KalimbaMessages.FAILED_TOGGLE, KalimbaIcons.BROKEN)
+            rumps.alert(*alert_args)
+            self.__update_status_menu_title(*alert_args)
         finally:
             self.__toggle_menu.reset()
-
-    def __toggle_colima(self, cmd: str) -> None:
-        try:
-            result = subprocess.run(["colima", cmd])
-            result.check_returncode()
-        except Exception as error:
-            logger.error(error)
-            rumps.alert("failed to toggle colima...", "🤕")
 
     def __update_status_menu_title(self, title: str, icon: str) -> None:
         self.__status_menu.title = icon + " " + title
@@ -101,16 +144,11 @@ class KalimbaMenuBarApp(rumps.App):
     def __update_colima_run_status(self, colima_running: bool) -> None:
         self.__colima_running = colima_running
 
-    def __handle_error(self, error: Exception) -> None:
-        logger.error(error)
-        self.__update_status_menu_title("failed to get status...", "🤕")
-        self.__update_colima_run_status(False)
-
 
 # mem(docs-link): update README if this changes.
 def run_kalimba(verbose=False):
     logger.setLevel(l.DEBUG if verbose else l.INFO)
-    logger.info("Starting the kalimba app... 🎶")
+    logger.info(KalimbaMessages.APP_STARTING)
 
     with KalimbaMenuBarApp.prepare_cli_pool() as pool:
         KalimbaMenuBarApp(pool).run()
